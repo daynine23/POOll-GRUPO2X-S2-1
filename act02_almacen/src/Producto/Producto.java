ea778459b29fa9ae77f6a0b9c86a931d3b000720
daynine23 <kevinalmeyda17@gmail.com>
2024-09-02 11:29:33 -0500

    first push of Product Program

---
+++ act02_almacen/src/Producto/Producto.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Producto;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Producto {
+    
+    private String nombreProducto;
+    private int cantidad;
+
+    public Producto() {
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+    
+}
